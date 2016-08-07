424bba84825c69682702e518084787b1d0a48d93
yonghwankim's macbook <yonghwan0330@gmail.com>
2016-08-07 16:27:52 +0900

    encode, decode method

--- Memo2/Memo2/Memo.m
+++ Memo2/Memo2/Memo.m
@@ -9,5 +9,22 @@
 #import "Memo.h"
 
 @implementation Memo
+@synthesize title, memoText;
 
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    self.memoText = [aDecoder decodeObjectForKey:@"memo"];      //설명에는 memoText가 아니라 memo로 돼있는데 작성자 실수인듯
+    self.title = [aDecoder decodeObjectForKey:@"title"];
+    
+    return self;
+}
+
+-(instancetype)init {
+    self  = [super init];
+    if (self) {
+        [self setTitle:@""];
+        [self setMemoText:@""];
+    }
+    return self;
+}
 @end
