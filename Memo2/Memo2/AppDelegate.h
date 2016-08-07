dc802f60f1dcbe971ce13a577eae0b994ce66b70
yonghwankim's macbook <yonghwan0330@gmail.com>
2016-08-07 16:22:39 +0900

    create title, memo instance

---
+++ Memo2/Memo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memo2
+//
+//  Created by yonghwankim's macbook on 8/7/16.
+//  Copyright © 2016 yonghwankim's macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memo2/Memo2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Memo2
+//
+//  Created by yonghwankim's macbook on 8/7/16.
+//  Copyright © 2016 yonghwankim's macbook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
